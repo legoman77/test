cefb56e84a04b10336d15c93c3e4250a069be8c0
Rees Payne <rplegos@gmail.com>
2015-11-21 12:06:43 -0500

    save

---
+++ public/js/boot.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var Flappy = {};
+Flappy.Boot = function () {};
+Flappy.Boot.prototype = {
+  init: function () {
+  },
+
+  preload: function () {
+  },
+
+  create: function () {
+    game.stage.backgroundColor = '#00FF00';
+  }
+
+};
